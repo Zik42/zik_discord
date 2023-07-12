5456ce1ef3fc33dbb24572d111c9c7adc849e0c8
Zik <vladziku@gmail.com>
2023-07-12 08:31:42 +0300

    -

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:
-        channel = discord.utils.get(client.get_all_channels(), name='test')
+        channel = discord.utils.get(client.get_all_channels(), name='ziks-bot')
         await channel.send(f'{member.name} has joined the voice channel {after.channel.name}')
 
 @client.event
