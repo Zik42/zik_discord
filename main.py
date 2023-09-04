9d2523facf7d18427203ae591334a39783d66ce4
Zik <vladziku@gmail.com>
2023-09-04 20:15:16 +0300

    cleanup

--- main.py
+++ main.py
@@ -10,21 +10,11 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 @client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:
         channel = discord.utils.get(client.get_all_channels(), name='ziks-bot')
         await channel.send(f'{member.name} has joined the voice channel {after.channel.name}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
 
 client.run(os.getenv("TOKEN"))
